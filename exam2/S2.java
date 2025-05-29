3fc019c8991b6ec39b7856cb60e1a779e5cbffdc
dalmaru-86 <tlstmdghk2@naver.com>
2025-05-29 17:29:39 +0900

    Add files via upload

---
+++ exam2/S2.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package exam;
+
+/**
+ * @author user
+ *	* 지시한 사항 외에 코딩은 자유롭게 하세요
+     2. a변수가 19 이상이면 성인 아니면 미성년자라고 화면에 표시하세요 
+    단, a 변수에는 19가 있습니다.
+    결과:  성인
+ */
+public class S2 {
+	public static void main(String[] args) {
+		int a=19;
+		
+		if(a>=19) {
+			System.out.println("성인");
+		}else {
+			System.out.println("미성년자");
+		}
+	}
+}
---
+++ exam2/S3.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package exam;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author user
+ * 3. 첫줄에 숫자의 총개수 N이 입력됩니다. 
+   2줄에 N개의 숫자가 랜덤하게 입력됩니다. 
+   오름차순 정렬해서 화면에 표시하는 코드를 작성하세요 
+    입력: 5
+    5 2 3 4 1
+   출력 1 2 3 4 5
+ */
+public class S3 {
+	public static void main(String[] args) {
+		Scanner scanner =new Scanner(System.in);
+		
+		int N=scanner.nextInt();
+		
+		int[] num=new int[N];
+		for (int i = 0; i < N; i++) {
+			num[i]=scanner.nextInt();
+		} Arrays.sort(num);
+		for (int n:num) {
+			System.out.println(n+" ");
+		}
+	}
+}
